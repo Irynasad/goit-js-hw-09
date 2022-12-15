bc5338d765672e80fbe8b85bb1fa27c161761a55
Iryna Sadychenko <108194331+Irynasad@users.noreply.github.com>
2022-12-16 01:18:54 +0200

    update 2task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,13 +4,7 @@ import 'flatpickr/dist/flatpickr.min.css';
 import toastr from 'toastr';
 import 'toastr/build/toastr.min.css';
 
-// toastr.error(
-//   'I do not think that word means what you think it means.',
-//   '"Please choose a date in the future"'
-// );
-
-// Command: toastr['error']('I do not think that means what you think it means.');
-
+//налаштування нотіфікацій
 toastr.options = {
   closeButton: true,
   debug: false,
@@ -29,26 +23,18 @@ toastr.options = {
   hideMethod: 'fadeOut',
 };
 
-// 0. дефолтний стан- кнопка Старт неактивна
-// let timerId = 0
-// let userDate = 0
-// const ...btnStart.setAttribute('disabled', true)
-
-// 1. вибір дати(const userDate)
-// 1.0 на інпут вішаємо слухача input та колбек onUserDate
-// 1.1.ерор-повідомлення, якщо вибрана дата в минулому
-// 1.2.дата майбутнього (>= new Date ())
-// 1.3.кнопка  Старт стає активною.
-
 const refs = {
-  btnStart: document.querySelector('button[data-start'),
   input: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('button[data-start'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 
 refs.btnStart.setAttribute('disabled', true);
 let userDate = null;
-let timerId = null;
-console.log(refs.input);
+// let timerId = null;
 
 const options = {
   enableTime: true,
@@ -57,23 +43,81 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     selectedDates[0] <= options.defaultDate
-      ? toastr.error(
-          'I do not think that word means what you think it means.',
-          '"Please choose a date in the future"'
-        )
+      ? toastr.error('Please choose a date in the future')
       : refs.btnStart.removeAttribute('disabled');
     userDate = selectedDates[0];
   },
 };
 
-const onUserDate = () => {
-  return (userDate = refs.input.value);
-};
+flatpickr(refs.input, options);
 
-refs.input.addEventListener('input', onUserDate);
+//в параметри Timer добавити результат флетріку(юзерДате)
+class Timer {
+  constructor({ onTick, userDate }) {
+    this.timerId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+  }
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    console.log(userDate);
+    //замість Date.now() * 1.0000002 поставити userDate
+    const startTime = Date.now() * 1.0000002;
+    this.isActive = true;
 
-// 2. Нажимаєм кнопку старт - запускається лічильник
-// 2.1.Вішаємо слухача на кнопку, в колбєк ставим роботу ліччильника
+    this.timerId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+      if (deltaTime <= 0) {
+        return;
+      }
 
-// 3. Лічильник = userDate.getTime? - Date.now()
-// 2.2 привести до одного формату!
+      const { days, hours, minutes, seconds } = this.convertMs(deltaTime);
+
+      this.onTick({ days, hours, minutes, seconds });
+
+      console.log(`${days}::${hours}::${minutes}::${seconds}`);
+    }, 1000);
+  }
+
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = this.addLeadingZero(Math.floor(ms / day), 3);
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour), 2);
+    const minutes = this.addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute),
+      2
+    );
+    const seconds = this.addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second),
+      2
+    );
+
+    return { days, hours, minutes, seconds };
+  }
+
+  addLeadingZero(value, num) {
+    return String(value).padStart(num, '0');
+  }
+}
+
+const timer = new Timer({
+  onTick: updateClockFace,
+});
+
+timer.start();
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
+
+refs.btnStart.addEventListener('click', timer.start.bind(timer));
